47a7beff64ae569776d006256c536dc50161231b
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 17:02:14 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioCincoListaUm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioCincoListaUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salário, Desconto e Impostos");
+            Console.WriteLine("=============================");
+            do
+            {
+                
+                //Não sei pq o Double não funciona ;-; 
+                const decimal valorAumento = 0.15m;
+                const decimal impostos = 0.8m;
+
+                decimal salarioAtual, salarioComAumento, salarioFinal;
+
+                Console.Write("Digite o seu salário atual: ");
+                salarioAtual = Convert.ToDecimal(Console.ReadLine());
+
+                salarioComAumento = salarioAtual + salarioAtual * valorAumento;
+
+                salarioFinal = salarioComAumento - salarioComAumento * impostos;
+
+                Console.WriteLine("\nO salário com aumento é: " + salarioComAumento);
+                Console.WriteLine("O salário final é: " + salarioFinal);
+
+            }while(true);
+        }
+    }
+}
